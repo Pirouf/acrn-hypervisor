e216bf9279181c156cb3075742511fbb94d4243f
Helmut Buchsbaum <helmut.buchsbaum@opensource.tttech-industrial.com>
2020-05-17 11:22:41 +0200

    Add debian packaging for release_1.6
    Signed-off-by: Helmut Buchsbaum <helmut.buchsbaum@opensource.tttech-industrial.com>

---
+++ debian/run-docker-build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# helper script to build ACRN with docker
+
+TOPDIR=$(git rev-parse --show-toplevel)
+DOCKER=$(which docker)
+
+if [ -z "${TOPDIR}" ]; then
+    echo "Run $0 from inside git repository!"
+    exit 1
+fi
+
+if [ -z "${DOCKER}" ]; then
+    echo "Cannot find docker binary, please install!"
+    exit 1
+fi
+
+pushd ${TOPDIR} >/dev/null
+
+if [ ! -f debian/docker/Dockerfile ]; then
+    echo "No Dockefile available!"
+    exit 1
+fi
+
+set -e
+${DOCKER} build -q -f debian/docker/Dockerfile -t acrn-hypervisor-build debian/docker
+${DOCKER} run --rm -u $(id -u):$(id -g) -v $(pwd):/acrn-hypervisor acrn-hypervisor-build
+
+popd >/dev/null
